a0d2df33cf50744e03a94e1297e5dccd142c1d14
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2019-07-07 16:50:45 +0100

    Update req_file.py

--- req_file.py
+++ req_file.py
@@ -12,3 +12,4 @@ data = json.loads(req_text)
 print(data['name'])
 
 lec fingerprint = 3a28ab5c879d24d67ce89f5f51a64f945b5967f0dab48510db4ab727130d6e61
+stu = 59ea4846251468183bd6097afff2aa1479868fc71ef19348c8ca1964cccb5349
