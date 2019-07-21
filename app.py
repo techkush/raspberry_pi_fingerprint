1069a056a305549b1279ad69f72d2066d6682cab
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2019-07-22 00:26:19 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, json, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/form_example', methods=['POST'])
+def form_example():
+
+    if request.method == 'POST':
+        req_data = request.get_json()
+        index = req_data['index']
+        print(index)
+        return 'Index Number Sent!' 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
